6c247e93eb762c024164d60a6e22237184be0d09
vanevane <vane.27.04@hotmail.com>
2015-06-18 01:42:44 -0400

    Index modifications

---
+++ front-end/app/js/lib/sidebar_menu.js
@@ -0,0 +1,46 @@
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+$("#menu-toggle-2").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled-2");
+    $('#menu ul').hide();
+});
+
+function initMenu() {
+    $('#menu ul').hide();
+    //$('#menu ul').children('.current').parent().show();
+    //$('#menu ul:first').show();
+    $('#menu li a').click(
+        function() {
+            var checkElement = $(this).next();
+            if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+                return true;
+            }
+            if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+                //$('#menu ul:visible').slideUp('normal');
+                checkElement.slideDown('normal');
+                return false;
+            }
+        }
+    );
+
+
+    var last=$.cookie('activeAccordionGroup');
+    if (last!=null) {
+        //remove default collapse settings
+        $("#accordion2 .collapse").removeClass('in');
+        //show the last visible group
+        $("#"+last).collapse("show");
+    }
+
+
+
+}
+$(document).ready(function() {initMenu();});
+
+$("#accordion2").bind('shown', function() {
+    var active=$("#accordion2 .in").attr('id');
+    $.cookie('activeAccordionGroup', active)
+});
